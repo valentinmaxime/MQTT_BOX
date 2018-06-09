a7e86811d5346ad901e821ce127084b8edeb1e72
unknown <valentin.m.dupont@gmail.com>
2018-06-09 18:16:49 +0200

    wifi

--- src/Conf/wifi.h
+++ src/Conf/wifi.h
@@ -1,8 +1,8 @@
 /**
  * @Date:   2018-06-08T15:10:17+02:00
- * @Last modified time: 2018-06-08T15:53:41+02:00
+ * @Last modified time: 2018-06-09T18:16:39+02:00
  */
 
  /* change it with ssid-password */
- const char* ssid = "Camion wifi nsa";
- const char* password = "vibestyle92";
+ const char* ssid = "***";
+ const char* password = "***";
